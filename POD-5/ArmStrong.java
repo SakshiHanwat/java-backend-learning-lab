d697436dfeafeaba0c32d969c96e7bf54e8fa8a0
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-11-11 05:24:18 +0000

    java

---
+++ POD-5/ArmStrong.java
@@ -0,0 +1,21 @@
+/*POD 5. Program for Armstrong Numbers
+
+Given a number x, determine whether the given number is Armstrong's number or not. A positive integer of n digits is called an Armstrong number of order n (order is the number of digits) if
+
+abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + ....
+
+Here a, b, c and d are digits of input number abcd.....
+
+Examples
+
+Input: n = 153
+Output: true
+Explanation: 153 is an Armstrong number, 111 + 555 + 333 = 153
+
+Input: n = 9474
+Output: true
+Explanation: 94 + 44 + 74 + 44 = 6561 + 256 + 2401 + 256 = 9474
+
+Input: n = 123
+Output: false
+Explanation: 1³ + 2³ + 3³ = 1 + 8 + 27 = 36*/
