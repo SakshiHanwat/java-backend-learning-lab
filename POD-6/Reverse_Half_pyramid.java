1caa09bf17d6e498e15dc93211105a3e6cc5dcef
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-11-11 09:58:38 +0000

    java

--- POD-6/Reverse_Half_pyramid.java
+++ POD-6/Reverse_Half_pyramid.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Reverse_Half_pyramid {
     public static void Print_Reverse_Half_pyramid(int num){
-        for(int i = 0; i <= num; i++){
-            for(int j = i; j<num; j++){
+        for(int i = 1; i <= num; i++){
+            for(int j = i; j<=num; j++){
                 System.out.print(" * ");
             }
             System.out.println();
