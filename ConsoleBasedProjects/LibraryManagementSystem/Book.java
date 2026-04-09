74bb9c528e649839c28482531955ebf68151daff
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2026-04-09 13:01:10 +0530

    Add files via upload

---
+++ ConsoleBasedProjects/LibraryManagementSystem/Book.java
@@ -0,0 +1,30 @@
+package LibraryManagementSystem;
+
+import java.util.Scanner;
+
+public class Book {
+
+    public int sNo;
+    public String bookName;
+    public String authorName;
+    public int bookQty;
+    public int bookQtyCopy;
+
+    Scanner sc = new Scanner(System.in);
+
+    public Book(){
+        System.out.println("Enter Serial No. Of Book: ");
+        this.sNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter Book Nmae: ");
+        this.bookName = sc.nextLine();
+
+        System.out.println("Enter Author Name: ");
+        this.authorName = sc.nextLine();
+
+        System.out.println("Enter Quantity of Books: ");
+        this.bookQty = sc.nextInt();
+        bookQtyCopy = this.bookQty;
+    }
+}
